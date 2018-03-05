a79bb40bffcc9906eb5ed04a8346f2d92ea8cd45
Pat Condon <patcondon@fancypants.local>
2018-03-05 14:16:49 -0500

    post a question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,4 +7,28 @@ class QuestionsController < ApplicationController
     @question = Question.find_by(id: params[:id])
     @errors = []
   end
+
+  def new
+    @question = Question.new
+    @errors =[]
+  end
+
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @question, notice: 'Question was successfully created.'
+    else
+      @errors = @question.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :description)
+  end
+
+
 end
